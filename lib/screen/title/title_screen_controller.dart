0c2e98d60478582d54714a525edb7495323dad64
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-10-25 21:17:08 +0900

    add: inappreviewを追加

--- lib/screen/title/title_screen_controller.dart
+++ lib/screen/title/title_screen_controller.dart
@@ -1,11 +1,14 @@
 import 'package:bokete_maker_app/screen/home_ful/home_ful_screen.dart';
 import 'package:bokete_maker_app/screen/home_ful2/home_ful2_screen.dart';
 import 'package:bokete_maker_app/screen/home_ful3/home_ful3_screen.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class TitleScreenController extends GetxController {
+  final InAppReview inAppReview = InAppReview.instance;
+
   void onTap1() {
     Get.to(() => const HomeFulScreen());
   }
@@ -33,11 +36,20 @@ class TitleScreenController extends GetxController {
             ),
             CupertinoDialogAction(
               child: const Text("応援する"),
-              onPressed: () => Navigator.pop(context),
+              onPressed: () {
+                Navigator.pop(context);
+                _appReview();
+              },
             ),
           ],
         );
       },
     );
+  }
+
+  void _appReview() async {
+    if (await inAppReview.isAvailable()) {
+      inAppReview.requestReview();
+    }
   }
 }
