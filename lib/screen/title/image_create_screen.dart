050b8f808a9fff5f71833b98679316d6cbfd3be2
iseruuuuu <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>
2023-06-09 21:01:52 +0900

    📱 TextFieldを閉じられたり、UXを調整した

--- lib/screen/title/image_create_screen.dart
+++ lib/screen/title/image_create_screen.dart
@@ -29,129 +29,137 @@ class ImageCreateScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              child: const Center(
-                child: Text(
-                  '作成例',
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: const Center(
+                    child: Text(
+                      '作成例',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                //TODO 画像を追加する。
+                Image.asset('assets/example/example.png'),
+                const Text(
+                  'お題',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
                   ),
                 ),
-              ),
-            ),
-            //TODO 画像を追加する。
-            Image.asset('assets/example/example.png'),
-            const Spacer(),
-            const Text(
-              'お題',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              child: TextField(
-                onChanged: controller.onTitleChanged,
-                maxLines: 2,
-                maxLength: 40,
-                inputFormatters: [
-                  TextInputFormatter.withFunction(
-                      (TextEditingValue oldValue, TextEditingValue newValue) {
-                    int numLines = '\n'.allMatches(newValue.text).length;
-                    if (numLines > 1) {
-                      return oldValue;
-                    }
-                    return newValue;
-                  }),
-                ],
-                minLines: 1,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: TextField(
+                    onChanged: controller.onTitleChanged,
+                    maxLines: 2,
+                    maxLength: 40,
+                    inputFormatters: [
+                      TextInputFormatter.withFunction(
+                          (TextEditingValue oldValue,
+                              TextEditingValue newValue) {
+                        int numLines = '\n'.allMatches(newValue.text).length;
+                        if (numLines > 1) {
+                          return oldValue;
+                        }
+                        return newValue;
+                      }),
+                    ],
+                    minLines: 1,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
                       color: Colors.black,
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            const Text(
-              'サブタイトル',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10,
-                vertical: 20,
-              ),
-              child: TextField(
-                onChanged: controller.onSubTitleChanged,
-                maxLines: 2,
-                maxLength: 20,
-                inputFormatters: [
-                  TextInputFormatter.withFunction(
-                      (TextEditingValue oldValue, TextEditingValue newValue) {
-                    int numLines = '\n'.allMatches(newValue.text).length;
-                    if (numLines > 1) {
-                      return oldValue;
-                    }
-                    return newValue;
-                  }),
-                ],
-                minLines: 1,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black,
+                const Text(
+                  'サブタイトル',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                  child: TextField(
+                    onChanged: controller.onSubTitleChanged,
+                    maxLines: 2,
+                    maxLength: 20,
+                    inputFormatters: [
+                      TextInputFormatter.withFunction(
+                          (TextEditingValue oldValue,
+                              TextEditingValue newValue) {
+                        int numLines = '\n'.allMatches(newValue.text).length;
+                        if (numLines > 1) {
+                          return oldValue;
+                        }
+                        return newValue;
+                      }),
+                    ],
+                    minLines: 1,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
                       color: Colors.black,
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: const BorderSide(
-                      color: Colors.blue,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.blue,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+                AppButton(
+                  onTap: controller.onSubmit,
+                  title: '生成する',
+                  buttonColor: Colors.yellow,
+                  titleColor: Colors.black,
+                ),
+              ],
             ),
-            AppButton(
-              onTap: controller.onSubmit,
-              title: '生成する',
-              buttonColor: Colors.yellow,
-              titleColor: Colors.black,
-            ),
-            const Spacer(),
-          ],
+          ),
         ),
       ),
     );
